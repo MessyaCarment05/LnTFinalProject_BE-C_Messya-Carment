3116901127ee3122a13597d5752781fad65b8956
MessyaCarment05 <messyacarmentp@gmail.com>
2023-05-08 20:29:48 +0700

    Membuat Validasi Admin dan User

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,16 +15,7 @@
                   <a class="nav-link active" aria-current="page" href="/dashboard">Dashboard</a>
                 </li>
               </ul> 
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="/create-barang">Add Product</a>
-                </li>
-              </ul> 
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="/create-category">Add Category</a>
-                </li>
-              </ul> 
+              
               <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
                   <a class="nav-link active" aria-current="page" href="/create-faktur">Faktur</a>
